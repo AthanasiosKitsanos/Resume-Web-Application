1f83d71067e4299e22071ac7cb0f62b0d7b377e6
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-23 21:16:33 +0200

    Created a User class

---
+++ ResumeProject/Models/User.cs
@@ -0,0 +1,27 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ResumeProject.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+
+    [Required]
+    public string? FirstName { get; set; }
+
+    [Required]
+    public string? LastName { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+
+    public int Age => DateTime.Now.Year - DateOfBirth.Year;
+
+
+    [EmailAddress]
+    public string? Email { get; set; }
+
+    public UserAccount? UserAccount{ get; set; }
+}
