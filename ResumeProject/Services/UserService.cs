87f128983bda99d4f3b09cd9aea1f1c41424f798
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-26 18:03:45 +0200

    Added a Service for creating, managing and validating users

--- ResumeProject/Services/UserService.cs
+++ ResumeProject/Services/UserService.cs
@@ -1,8 +1,20 @@
-using System;
+using Microsoft.AspNetCore.Identity;
+using ResumeProject.Models;
 
 namespace ResumeProject.Services;
 
 public class UserService: IUserService
 {
-    
+    private readonly UserManager<ApplicationUser> _userManager; // Used for creating, managing and validating users
+
+    public UserService(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IdentityResult> RegisterUserAsync(ApplicationUser user, string password)
+    {
+        var result = await _userManager.CreateAsync(user, password); // We use CreateAsync to create a new user.
+        return result;
+    }
 }
