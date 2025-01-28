753a34a5a5a117baf1bc76f8151d332ac12a3dd6
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-28 22:51:57 +0200

    Created controllers for Http requests, methods are not yet written

---
+++ ResumeProject/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProject.Controllers;
+
+// This controller manages the http requests fot logging in, registration and logging out
+
+[ApiController]
+[Route("api/[controller]")]
+public class AccountController: ControllerBase
+{
+    //[HttpPost("register")]
+    //public async Task<IActionResult> Register([FromBody])
+
+    // Todo, complete the above
+    // create HttpPost for login and for logout
+}
---
+++ ResumeProject/Controllers/UserControllers.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProject.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserControllers: ControllerBase    
+{
+    // Todo, create Update and Delete methods
+}
