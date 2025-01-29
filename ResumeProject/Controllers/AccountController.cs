ddb438c405c8f40ea642093e4395b310c79eccd9
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-29 20:09:57 +0200

    Added the methods that use the Services

--- ResumeProject/Controllers/AccountController.cs
+++ ResumeProject/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using ResumeProject.Models;
+using ResumeProject.Services;
 
 namespace ResumeProject.Controllers;
 
@@ -9,9 +11,53 @@ namespace ResumeProject.Controllers;
 [Route("api/[controller]")]
 public class AccountController: ControllerBase
 {
-    //[HttpPost("register")]
-    //public async Task<IActionResult> Register([FromBody])
+    private readonly AccountServices _accountServices;
 
-    // Todo, complete the above
-    // create HttpPost for login and for logout
+    public AccountController(AccountServices accountServices)
+    {
+        _accountServices = accountServices;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterUserDTO model)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = model.Email,
+            Email = model.Email,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            DateOfBirth = DateTime.Now
+        };
+
+        var result = await _accountServices.RegisterUserAsync(user, model.Password);
+
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok("Account has been created!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> LogIn([FromBody] LogInDTO model)
+    {
+        var result = await _accountServices.LoginUserAsync(model.Email, model.Password, model.RememberMe);
+
+        if(!result.Succeeded)
+        {
+            return Unauthorized("Invalide Username or Password, please try again");
+        }
+
+        return Ok("You are logged in");
+    }
+
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await _accountServices.LogOutUser();
+
+        return Ok("Logged out succesfully");
+    }
 }
--- ResumeProject/Controllers/UserControllers.cs
+++ ResumeProject/Controllers/UserControllers.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using ResumeProject.Models;
+using ResumeProject.Services;
 
 namespace ResumeProject.Controllers;
 
@@ -7,5 +9,45 @@ namespace ResumeProject.Controllers;
 [Route("api/[controller]")]
 public class UserControllers: ControllerBase    
 {
-    // Todo, create Update and Delete methods
+    private readonly UserService _userService;
+
+    public UserControllers(UserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPut("update-info")]
+    public async Task<IActionResult> Update([FromBody] UpdateUserDTO model)
+    {
+        var user = new ApplicationUser
+        {
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+            Email = model.Email,
+            UserName = model.Email,
+            PhoneNumber = model.PhoneNumber
+        };
+
+        var result = await _userService.UpdateUserAsync(user);
+
+        if(!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok("User info updated succesfully");
+    }
+
+    [HttpDelete("delete")]
+    public async Task<IActionResult> Delete([FromBody] string id)
+    {
+        var result = await _userService.DeleteUserAsync(id);
+
+        if(!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok("Account deleted succesfully");
+    }
 }
