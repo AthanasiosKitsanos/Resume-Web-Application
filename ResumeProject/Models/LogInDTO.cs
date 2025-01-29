3690f9a437b867d0d9d08bd1fc043c10451facdd
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-29 21:52:26 +0200

    used "?" to remove the editors errors for string types

--- ResumeProject/Models/LogInDTO.cs
+++ ResumeProject/Models/LogInDTO.cs
@@ -4,7 +4,7 @@ namespace ResumeProject.Models;
 
 public class LogInDTO
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
     public bool RememberMe { get; set; }
 }
--- ResumeProject/Models/UpdateUserDTO.cs
+++ ResumeProject/Models/UpdateUserDTO.cs
@@ -4,8 +4,8 @@ namespace ResumeProject.Models;
 
 public class UpdateUserDTO
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
 }
