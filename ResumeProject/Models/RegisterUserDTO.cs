8441b23d8607ed6c52f7552d9f6e857c3f647b8f
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-29 20:10:31 +0200

    Swapped the properties, because they were the other way around

--- ResumeProject/Models/RegisterUserDTO.cs
+++ ResumeProject/Models/RegisterUserDTO.cs
@@ -4,7 +4,10 @@ namespace ResumeProject.Models;
 
 public class RegisterUserDTO
 {
+    public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public bool rememberMe { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
 }
