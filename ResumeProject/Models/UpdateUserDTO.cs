d1e4d6b6a6480b6e16bcb50efd3a17b4c648551c
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-01-29 20:13:14 +0200

    Created safety properties to be used in the update method inside the UserController

---
+++ ResumeProject/Models/UpdateUserDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResumeProject.Models;
+
+public class UpdateUserDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+}
