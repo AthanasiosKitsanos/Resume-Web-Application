6408f575a1cd0f7102d726e543e27fa417567a67
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-02-02 11:57:58 +0200

    Added the login logic

--- ResumeProject/Pages/Account/Login.cshtml.cs
+++ ResumeProject/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResumeProject.Services;
+using ResumeProject.Models;
 
 namespace ResumeProject.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private readonly AccountServices _accountServices;
+
+        public LoginModel(AccountServices accountServices)
+        {
+            _accountServices = accountServices;
+        }
+
+        [BindProperty]
+        public LogInDTO LoginDto  { get; set; } = new LogInDTO();
+
         public void OnGet()
         {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var token = await _accountServices.LoginUserAsync(LoginDto.Email!, LoginDto.Password!, LoginDto.RememberMe);
+
+            if(token is null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid email or password, please try again");
+            }
+
+            return RedirectToPage("/Index");
         }
     }
 }
