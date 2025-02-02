2417f70d7b1744df75f041e3c7336e976894e563
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-02-02 11:30:21 +0200

    Fixed some issues with the RegisterModel

--- ResumeProject/Pages/Account/Register.cshtml.cs
+++ ResumeProject/Pages/Account/Register.cshtml.cs
@@ -3,38 +3,54 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using ResumeProject.Models;
 using ResumeProject.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ResumeProject.Pages.Account
 {
     public class RegisterModel : PageModel
     {
-        private readonly AccountServices? _accountServices;
+        private readonly AccountServices _accountServices;
 
         public RegisterModel(AccountServices accountServices)
         {
             _accountServices = accountServices;
         }
+
+        
         [BindProperty]
-        public RegisterUserDTO  RegisterDTO{ get; set; } = new();
-        public void OnGet() {}
+        public RegisterUserDTO RegisterDto { get; set; } = new RegisterUserDTO();
+
+        public void OnGet()
+        {
+
+        }
+
 
         public async Task<IActionResult> OnPostAsync()
         {
             if(!ModelState.IsValid)
             {
+                foreach(var ModelStateEntry in ModelState.Values)
+                {
+                    foreach(var error in ModelStateEntry.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.ErrorMessage);
+                    }
+                }
+                
                 return Page();
             }
 
             var user = new ApplicationUser
             {
-                UserName = RegisterDTO.Email,
-                Email = RegisterDTO.Email,
-                FirstName = RegisterDTO.FirstName,
-                LastName = RegisterDTO.LastName,
-                DateOfBirth = RegisterDTO.DateOfBirth
+                UserName = RegisterDto.Email,
+                Email = RegisterDto.Email,
+                FirstName = RegisterDto.FirstName,
+                LastName = RegisterDto.LastName,
+                DateOfBirth = RegisterDto.DateOfBirth
             };
     
-            var result = await _accountServices.RegisterUserAsync(user, RegisterDTO.Password!);
+            var result = await _accountServices.RegisterUserAsync(user, RegisterDto.Password!);
 
             if(!result.Succeeded)
             {
