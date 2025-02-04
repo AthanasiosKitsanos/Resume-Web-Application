e3fb5acd72a4959eaedc8ab9ba3d7be076be4cca
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-02-04 19:15:35 +0200

    Created the page for the users info update

---
+++ ResumeProject/Pages/Account/Update.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ResumeProject.Pages.Account
+{
+    public class UpdateModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+        }
+    }
+}
