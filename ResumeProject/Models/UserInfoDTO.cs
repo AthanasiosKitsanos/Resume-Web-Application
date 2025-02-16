51e84c3762355b8b520e100a2ca14835887fe8a4
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-02-16 14:20:37 +0200

    UserInfoDTO for getting specific elements of the database table columns

---
+++ ResumeProject/Models/UserInfoDTO.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ResumeProject.Models;
+
+public class UserInfoDTO
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set;} = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
