06e9588ef8791d933e05c92e45aa777df94ec434
Athanasios Kitsanos <kitsanos.dev@gmail.com>
2025-02-16 14:23:02 +0200

    The pagemodel of the profile page

---
+++ ResumeProject/Pages/Account/MyProfile.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResumeProject.Services;
+using ResumeProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ResumeProject.Pages.Account
+{
+    public class MyProfileModel : PageModel
+    {
+        private readonly UserInfoService _userInfoService;
+
+        public MyProfileModel(UserInfoService userInfoService)
+        {
+            _userInfoService = userInfoService;
+        }
+
+        public List<UserInfoDTO>? UserInfo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            // Fetch the logged-in user's information asychronously
+           UserInfo = await _userInfoService.GetLoggedInUserInfoAsync();
+
+           if(UserInfo is null || UserInfo.Count == 0)
+           {
+                return NotFound();
+           }
+
+           return Page();
+        }
+    }
+}
